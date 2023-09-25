c7e99e05195892e182d48dbca56978d6ed03cd93
Coder-King3 <2196592083@qq.com>
2023-09-25 09:18:06 +0800

    Update: router Architecture update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* 模块 */
 const Koa = require('koa')
 const app = new Koa()
+const Router = require('koa-router')
+const router = new Router()
 const cors = require('koa2-cors')
 const KoaStatic = require('koa-static')
 const path = require('path')
@@ -12,7 +14,7 @@ app.use(KoaStatic(path.resolve() + '/public'))
 
 /* 路由 */
 const initRoute = require('./router/index')
-initRoute(app)
+initRoute(app, router)
 
 /* 服务 */
 app.listen(PORT, () => {
--- router/index.js
+++ router/index.js
@@ -1,18 +1,25 @@
-const Router = require('koa-router')
-const router = new Router()
+function registerRoute(router) {
+  router.get('/welcome', async (ctx, next) => {
+    let url = ctx.url
+    ctx.body = {
+      url,
+      message: `你好，你当前访问的接口是: '/welcome' `
+    }
+    next()
+  })
+}
 
-router.get('/welcome', async (ctx, next) => {
-  let url = ctx.url
+function initRoute(app, router) {
+  /* 全局中间件 */
+  app.use(function (ctx, next) {
+    console.log(`requst: { url: '${ctx.url}' }`)
+    next()
+  })
 
-  ctx.body = {
-    url,
-    message: `你好，你当前访问的接口是: '/welcome' `
-  }
-  next()
-})
-
-function initRoute(app) {
   // 注册路由
+  registerRoute(router)
+
+  // 挂载路由
   app.use(router.routes())
 }
 
